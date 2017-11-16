2a6355a3c5c2e8f907995564f2851bdf5a8f660a
790013438 <790013438@qq.com>
2017-11-16 16:54:27 +0800

    添加忽略文件/out/,重命名类名
    重命名类名，因为要警告拼写

--- src/main/java/snippets/MasterspringmvcApplication.java
+++ src/main/java/snippets/MasterspringmvcApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MasterspringmvcApplication {
+public class MasterSpringMvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MasterspringmvcApplication.class, args);
+        SpringApplication.run(MasterSpringMvcApplication.class, args);
     }
 }
--- src/test/java/snippets/MasterspringmvcApplicationTests.java
+++ src/test/java/snippets/MasterspringmvcApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MasterspringmvcApplicationTests {
+public class MasterSpringMvcApplicationTests {
 
     @Test
     public void contextLoads() {
