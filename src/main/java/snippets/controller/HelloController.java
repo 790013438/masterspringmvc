06b3a636beaad5b9eda4273efc14f5aec4a33654
790013438 <790013438@qq.com>
2017-11-20 15:12:16 +0800

    Passing data to the view

--- src/main/java/snippets/controller/HelloController.java
+++ src/main/java/snippets/controller/HelloController.java
@@ -1,13 +1,15 @@
 package snippets.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String hell() {
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from the controller");
         return "resultPage";
     }
 }
