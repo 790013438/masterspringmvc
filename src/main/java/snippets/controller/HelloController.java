89a8299d51eb7f4fe6e9e250daeac866c45239ed
790013438 <790013438@qq.com>
2017-11-20 21:25:31 +0800

    从浏览器获得数据

--- src/main/java/snippets/controller/HelloController.java
+++ src/main/java/snippets/controller/HelloController.java
@@ -3,13 +3,14 @@ package snippets.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello from the controller");
+    public String hello(@RequestParam(defaultValue = "world", value="name") String userName, Model model) {
+        model.addAttribute("message", "Hello from the controller， " + userName);
         return "resultPage";
     }
 }
